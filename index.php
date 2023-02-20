0da0b3a109fcdc9e69b2e2967cc214d9db1d6772
Luca-Giudice <2giudice4@gmail.com>
2023-02-20 16:39:02 +0100

    genere

--- index.php
+++ index.php
@@ -1,14 +1,34 @@
 <?php
 
+class Genere
+{
+   public $name;
+   
+   public function __construct($name)
+   {
+    $this->name = $name;
+   }
+}
 class Movie {
+    public $title;
     public $genere;
     public $lingua;
 
 
 
-    public function __construct($genere,$lingua)
+    public function __construct($title,$genere,$lingua)
     {
+        $this->title = $title; 
         $this->genere = $genere;
         $this->lingua = $lingua; 
     }
+
+    public function getDetails(){
+        $genere = $this->genere->name;
+        return "Titolo: $this->title Genere: $genere Lingua: $this->lingua";
+    }
 }
+
+$romantico = new Genere('Romantico');
+
+$casablanca = new Movie ('Casablanca', '2:30h', $romantico);
